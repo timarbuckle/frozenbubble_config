92ebd378862f61920bb7a1176591dbbce8a03b17
Tim Arbuckle <tim.arbuckle@openx.com>
2016-12-01 21:33:15 -0800

    first

---
+++ config/views.py
@@ -0,0 +1,31 @@
+from django.http import JsonResponse
+from config.models import Config
+
+def appconfig(request, device_id):
+    try:
+        config = Config.objects.get(device_id=device_id)
+    except Config.DoesNotExist:
+        config = Config.objects.create(device_id=device_id)
+    data = {
+        "device_id": config.device_id,
+        "cool_down": {
+            "enable": config.cool_down,
+            "max_sessions": config.max_sessions 
+        },
+        "native_theme": config.native_theme,
+        "rewarded_video": {
+            "rescue_video": config.rescue_video,
+            "lose_video": config.lose_video,
+            "win_video": config.win_video,
+            "preroll_video": config.preroll_video,
+            "resume_game_video": config.resume_game_video,
+            "app_startup_video": config.app_startup_video 
+        },
+        "sponsored_product": {
+            "enable": config.sponsored_enable,
+            "frequency": config.sponsored_frequency
+        },
+        "location_wallpaper": config.location_wallpaper 
+    }
+    return JsonResponse(data)
+
